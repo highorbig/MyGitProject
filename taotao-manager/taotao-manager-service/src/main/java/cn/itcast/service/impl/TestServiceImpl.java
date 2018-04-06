fb1187bfe745b4a9a94de131ba3d9d80ad93dea3
highorbig <37575781+highorbig@users.noreply.github.com>
2018-04-06 20:37:18 +0800

    更改方法

--- taotao-manager/taotao-manager-service/src/main/java/cn/itcast/service/impl/TestServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/cn/itcast/service/impl/TestServiceImpl.java
@@ -12,6 +12,7 @@ public class TestServiceImpl implements TestService {
     private TestMapper testMapper;
     @Override
     public String queryDate() {
+        int i=1;
         return this.testMapper.queryDate();
     }
 }
