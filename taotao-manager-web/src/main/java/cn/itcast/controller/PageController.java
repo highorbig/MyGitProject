a1278e48dfb844ffb8b898874906ea0117e3686a
highorbig <37575781+highorbig@users.noreply.github.com>
2018-04-07 09:03:35 +0800

    导入静态图片

---
+++ taotao-manager-web/src/main/java/cn/itcast/controller/PageController.java
@@ -0,0 +1,21 @@
+package cn.itcast.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("page")
+public class PageController {
+
+
+    /**
+     * 通用页面跳转方法
+     * @param pageName
+     * @return
+     */
+    @RequestMapping("{pageName}")
+    public String toPage(@PathVariable("pageName") String pageName){
+        return pageName;
+    }
+}
